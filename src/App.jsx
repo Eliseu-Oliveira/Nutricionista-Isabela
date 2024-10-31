84adcf637c275a2d7a3533623b3c2a26eaa89c1c
Eliseu-Oliveira <eliseu.germano@outlook.com>
2024-10-31 11:23:57 -0300

    Ajustes nos componentes e também exclusão de itens desnecessários

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import Header from "./components/Header";
-import PaymentSection from "./components/PaymentSection";
 import Banner from "./components/Banner";
 import Apresentacao from "./components/Apresentacao";
 import Features from "./components/Features";
-import InstagramSection from "./components/InstagramSection";
 import ConsultationSection from "./components/ConsultationSection";
 import Cards from "./components/Cards";
 import MetodoNutricional from "./components/MetodoNutricional";
-import GallerySlider from "./components/GallerySlider";
 import Depoimentos from "./components/Depoimentos";
 import WhatsAppIcon from "./components/WhatsAppIcon";
-import Chatbot from "./components/ChatBot";
-
 import Footer from "./components/Footer";
 import "./App.css";
 
@@ -22,15 +17,12 @@ function App() {
       <Banner />
       <Features />
       <Apresentacao />
-      {/* <InstagramSection /> */}
       <ConsultationSection />
       <treatmentCategory />
       <Cards />
-      <MetodoNutricional/>
-      {/* <GallerySlider /> */}
-      <Depoimentos/>
-      <WhatsAppIcon/>
-      {/* <Chatbot/> */}
+      <MetodoNutricional />
+      <Depoimentos />
+      <WhatsAppIcon />
       <Footer />
     </div>
   );
